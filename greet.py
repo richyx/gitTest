f46f98d511d2a06b1e6727488570b890b3a76e11
Richard Xue <richardxue22@gmail.com>
2018-07-01 23:13:13 -0700

    i fixed some formatting issues

--- greet.py
+++ greet.py
@@ -2,10 +2,6 @@ def greet(name):
     print('Hi', name, ', how are you doing?')
     print(' - Python')
 
-
-
-
-
 def joke():
     print('Did you know that an opinion without 3.14159 is just an onion?')
     
